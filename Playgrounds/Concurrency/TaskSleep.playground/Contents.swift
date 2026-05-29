1132fe6069eaaa60b7b4dafb82642be895fdff4d
feed0 <dogmas-gutters.07@icloud.com>
2026-05-29 16:45:09 -0300

    [Concurrency-00001] TaskSleep

---
+++ Playgrounds/Concurrency/TaskSleep.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+// MARK: - Definition
+
+func asyncPrint(_ string: String) async throws {
+    
+    /// Sleep
+    try await Task.sleep(for: .seconds(1))
+    print("1 sec")
+    
+    try await Task.sleep(for: .seconds(1))
+    print("2 secs")
+    
+    /// Finally
+    print(string)
+}
+
+// MARK: - Call
+
+Task {
+    do {
+        try await asyncPrint("End")
+    } catch {
+        /// not implemented
+    }
+}
